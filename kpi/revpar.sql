67c92812d752743b2b01b025818a7c2e6033fac1
MarCorri <mmc.corriero@gmail.com>
2026-04-01 12:41:06 +0200

    Update revpar.sql

--- kpi/revpar.sql
+++ kpi/revpar.sql
@@ -1,1 +1,8 @@
+-- Nome: RevPAR
+-- Descrizione: Revenue per Available Room
+-- Nota: richiede disponibilità camere (esempio semplificato)
 
+SELECT 
+    SUM(room_revenue) / SUM(nights) AS revpar
+FROM reservations
+WHERE status = 'Confirmed';
